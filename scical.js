bd5ee25338de1e3104ce8553a52351d310631750
divyasrita0918 <tpdivyasrita10@gmail.com>
2025-08-15 22:46:56 +0530

    validity check done

--- scical.js
+++ scical.js
@@ -3,21 +3,78 @@ const display = document.getElementById("input");
 const historyBox = document.getElementById("history");
 const historyEntries = document.getElementById("history-entries");
 const clearHistoryBtn = document.querySelector(".clear-history");
-const angleMode=document.querySelector('input[name="angleMode"]:checked');
+
 
 let history = [];
 
 
 function append(value) {
-  display.textContent += value;
+  display.value += value;
 }
 
-function clearDisplay() {
-  display.textContent = "";
-  displayResult.textContent = "0";
+function calculate() {
+  const angleMode=document.querySelector('input[name="angleMode"]:checked');
+  const expr = display.value;
+  if (!expr.trim()) return;
+  if (hasInvalidChars(expr)) {
+    displayResult.textContent = "Invalid";
+    return;
+  }
+  try {
+    const result = evaluate(expr);
+    displayResult.textContent = result;
+    addToHistory(expr, result);
+    display.value = "";
+  } catch {
+    displayResult.textContent = "Error";
+  }
+}
+
+function hasInvalidChars(expr) {
+    const validChars = /^[0-9+\-*/^%!().√πe\sA-Za-z]*$/;
+  if (!validChars.test(expr)) return true;
+
+  const allowedFunctions = ["sin","cos","tan","asin","acos","atan","ln","log"];
+  const matches = expr.match(/[A-Za-z]+/g) || [];
+  for (let fn of matches) {
+    if (!allowedFunctions.includes(fn)) return true;
+  }
+
+  return false;
+}
+
+function toggleHistory() {
+  if (historyBox.style.display === "flex") {
+    historyBox.style.display = "none";
+    clearHistoryBtn.style.display = "none";
+  } else {
+    historyBox.style.display = "flex";
+    clearHistoryBtn.style.display = "inline-block";
+  }
+}
+
+function addToHistory(expression, result) {
+  const entry = `${expression} = ${result}`;
+  history.push(entry);
+  const p = document.createElement("p");
+  p.textContent = entry;
+  historyEntries.appendChild(p);
+}
+
+
+function clearHistory() {
+  history = [];
+  historyEntries.innerHTML = "";
+  clearHistoryBtn.style.display = "none"; 
+  historyBox.style.display = "none";      
 }
 
 function deleteLast() {
-  const currentValue = display.textContent;
-  display.textContent = currentValue.slice(0, -1);
+  const currentValue = display.value;
+  display.value = currentValue.slice(0, -1);
+}
+
+function clearDisplay() {
+  display.value = "";
+  displayResult.textContent = "0";
 }
