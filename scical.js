2487439c6b304f109fbedcd0b16f7fce3a700e1d
divyasrita0918 <tpdivyasrita10@gmail.com>
2025-08-16 10:21:27 +0530

    mapping done

--- scical.js
+++ scical.js
@@ -31,17 +31,26 @@ function calculate() {
 }
 
 function hasInvalidChars(expr) {
-    const validChars = /^[0-9+\-*/^%!().√πe\sA-Za-z]*$/;
+  const validChars = /^[0-9+\-*/^%!().√πe\sA-Za-z]*$/;
   if (!validChars.test(expr)) return true;
 
-  const allowedFunctions = ["sin","cos","tan","asin","acos","atan","ln","log"];
-  const matches = expr.match(/[A-Za-z]+/g) || [];
-  for (let fn of matches) {
-    if (!allowedFunctions.includes(fn)) return true;
-  }
+  const allowedFunctions = [
+    "sin","cos","tan",
+    "asin","acos","atan",
+    "ln","log"
+  ];
 
-  return false;
+  const allowedConstants = ["e", "π"];
+
+  const allowedWords = new Set([...allowedFunctions, ...allowedConstants]);
+
+  const words = expr.match(/[A-Za-zπ]+/g) || [];
+  for (const w of words) {
+    if (!allowedWords.has(w)) return true; 
+  }
+  return false; 
 }
+
 
 function toggleHistory() {
   if (historyBox.style.display === "flex") {
@@ -77,4 +86,40 @@ function deleteLast() {
 function clearDisplay() {
   display.value = "";
   displayResult.textContent = "0";
+}
+
+//expression evaluation
+
+function evaluate(expr){
+  const tokensbeforemap=tokenize(expr);
+  const tokens=maptoken(tokensbeforemap);
+  if(tokens[0]==="-") tokens.unshift("0");
+  const postfixTokens = infixToPostfix(tokens);
+  const result = evaluatePostfix(postfixTokens);
+  return result;
+}
+
+function precedence(op){
+  if(op==="+" || op==="-") return 1;
+  if(op==="*" || op==="/") return 2;
+  if(op==="power") return 3;
+  if(op==="factorial" || op==="sqrt" || op==="sin" || op==="cos" || op==="tan" || op==="asin" || op==="acos" || op==="atan" || op==="sqrt" || op==="ln" || op==="log") return 4;
+  return 0;
+}
+function tokenize(expr){
+    const tokens = expr.match(
+    /(\d+(\.\d+)?)|[+\-*/^%!√()]|sin|cos|tan|asin|acos|atan|ln|log|π|e/g);
+    return tokens || [];
+}
+
+function maptoken(tokens){
+    const mapping={
+      '^': "power", '√':"sqrt",'π':3.14159265,'e':2.7182818, '!':"factorial"
+    }
+    for(let i=0;i<tokens.length;i++){
+      if(tokens[i]==='^' || tokens[i]==='√' || tokens[i]==='π' || tokens[i]==='e' || tokens[i]==='!') 
+        tokens[i]=mapping[tokens[i]];
+      else continue;
+    }
+    return tokens;
 }
