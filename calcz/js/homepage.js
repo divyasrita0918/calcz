b842d93fb8fb58d7d4f845116b714d4513a77db9
divyasrita0918 <tpdivyasrita10@gmail.com>
2025-09-25 18:29:43 +0530

    path

--- calcz/js/homepage.js
+++ calcz/js/homepage.js
@@ -1,12 +1,12 @@
 document.getElementById("btn1").addEventListener("click", function () {
-    window.location.href = "scical.html";
+    window.location.href = "../templates/scical.html";
 })
 document.getElementById("btn2").addEventListener("click", function () {
-    window.location.href = "bascal.html";
+    window.location.href = "../templates/bascal.html";
 })
 document.getElementById("btn3").addEventListener("click", function () {
-    window.location.href = "radix.html";
+    window.location.href = "../templates/radix.html";
 })
 document.getElementById("btn4").addEventListener("click", function () {
-    window.location.href = "currency.html";
+    window.location.href = "../templates/currency.html";
 })
